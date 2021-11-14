abab98a16c9ea4d9cb405e528187017ee4afc5ae
ehab-ahma <ehab24495@gmail.com>
2021-11-14 21:20:18 +0200

    done with cc26

---
+++ python/tests/test_insertion.py
@@ -0,0 +1,30 @@
+from python.insertion_blog import insertion_sort
+
+
+def test_insertion():
+    # arrange
+    ehab_list = [8, 4, 23, 42, 16, 15]
+    expected = [4, 8, 15, 16, 23, 42]
+    actuaehab_list = insertion_sort(ehab_list)
+    assert expected == actuaehab_list
+
+
+def test_insertion_2():
+    ehab_list = [20, 18, 12, 8, 5, -2]
+    excepted = [-2, 5, 8, 12, 18, 20]
+    actuaehab_list = insertion_sort(ehab_list)
+    assert excepted == actuaehab_list
+
+
+def test_insertion_3():
+    ehab_list = [5, 12, 7, 5, 5, 7]
+    excepted = [5, 5, 5, 7, 7, 12]
+    actuaehab_list = insertion_sort(ehab_list)
+    assert excepted == actuaehab_list
+
+
+def test_insertion_4():
+    ehab_list = [2, 3, 5, 7, 13, 11]
+    excepted = [2, 3, 5, 7, 11, 13]
+    actuaehab_list = insertion_sort(ehab_list)
+    assert excepted == actuaehab_list
